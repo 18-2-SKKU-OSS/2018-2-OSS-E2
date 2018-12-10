23ee2d47644f48d123abbf958757ae474d3773e0
mzyt3569 <43778699+mzyt3569@users.noreply.github.com>
2018-12-11 03:34:34 +0900

    Add 'isClick.cs'

---
+++ Assets/Scripts/isClick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class isClick : MonoBehaviour {
+    private bool isClicked = false;
+
+    private void Start()
+    {
+        isClicked = false;
+    }
+
+    public onClicked()
+    {
+        isClicked = true;
+        StartCoroutine("clickWait");
+    }
+
+    IEnumerator clickWait()
+    {
+        yield return new WaitForSeconds(0.5f);
+        isClicked = false;
+    }
+
+    public bool getIsClicked()
+    {
+        return isClicked;
+    }
+}
