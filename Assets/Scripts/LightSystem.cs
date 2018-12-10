d7269e80024acaa98da3bbe319227d2baaf27d83
mzyt3569 <43778699+mzyt3569@users.noreply.github.com>
2018-12-11 03:43:02 +0900

    Update LightSystem.cs
    필드 자료형 변경 및 기능 추가

--- Assets/Scripts/LightSystem.cs
+++ Assets/Scripts/LightSystem.cs
@@ -1,15 +1,29 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LightSystem : MonoBehaviour {
-    public Gameobject[] lights;
-    public Gameobject[] switches;
+    public SwitchLight[] lights;
+    public isClick[] switches;
 
     private void Start()
     {
         //만약, 스위치와 광원이 매칭되지 않는다면 Light System 종료
         if(lights.Length != switches.Length)
             gameObject.SetActive(false);
+    }
+
+    private void FixedUpdate()
+    {
+        for(int i=0; i < switches.Length; i++)
+        {
+            if (switches[i].getIsClicked())
+            {
+                if (lights[i].isTurnOn)
+                    lights[i].TurnOff();
+                else
+                    lights[i].TurnOn();
+            }
+        }
     }
 }
