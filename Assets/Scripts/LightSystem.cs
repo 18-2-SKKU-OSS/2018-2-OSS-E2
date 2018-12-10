b725b3b0c303f15003fe02278ee28dd1bef160e3
mzyt3569 <43778699+mzyt3569@users.noreply.github.com>
2018-12-11 03:16:39 +0900

    Add new script 'LightSystem.cs'

---
+++ Assets/Scripts/LightSystem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSystem : MonoBehaviour {
+    public Gameobject[] lights;
+    public Gameobject[] switches;
+
+    private void Start()
+    {
+        //만약, 스위치와 광원이 매칭되지 않는다면 Light System 종료
+        if(lights.Length != switches.Length)
+            gameObject.SetActive(false);
+    }
+}
